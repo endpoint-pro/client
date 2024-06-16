ef8eab3934d083098bdf0b3e340bfa42e6ef173f
Shail Pokharel <51349375+pokharelshail@users.noreply.github.com>
2024-06-16 13:29:55 -0400

    Json Box  Format on paste and style changes (#4)
    * Edit Json formatter to work on paste, onChange and be scrollable with line numbers
    
    * remove unused files and modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { useState } from "react";
 import JsonBox from "./client/components/JsonBox";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <JsonBox />
+    <div className="min-h-screen bg-gray-900 text-white">
+      <div className="max-w-4xl mx-auto p-4 h-full">
+        <JsonBox />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
